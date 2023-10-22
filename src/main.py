938fd1704ee35b48b69c8a438d0bf21edb9ed26d
João Pedro Viguini <70961838+jpviguini@users.noreply.github.com>
2023-10-22 07:42:26 -0300

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,1 +1,26 @@
+import cohere
+import streamlit as st
 
+co = cohere.Client("3lmg86xuofLDUK9UFR5mbvLNGwlVRHNw2IMjHeQm")
+
+st.title("Your mnemonic")
+
+title = st.text_input("Enter an industry:", "")
+#keywords = st.text_input("Enter an industry:", "")
+
+
+creativity_options = ["Eminem", "Soon..."]
+selected_creativity = st.selectbox("Select:", creativity_options)
+
+if selected_creativity == "Eminem":
+    artista = "Eminem"
+elif selected_creativity == "Soon...":
+    artista = Eminem
+else:
+    artista = Eminem
+
+if st.button("Generate Idea"):
+    st.subheader("Generated Mnmonic:")
+
+    st.write("Tema: ", title)
+    st.write("Artista: ", artista)
